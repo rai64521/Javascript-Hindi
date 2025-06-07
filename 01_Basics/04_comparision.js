d826206afe3abe8e153cb7aeab2a6c99e916eb7a
Sumant Rai <raisumant99@gmail.com>
2025-06-07 09:31:47 +0000

    datatype summary

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,32 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);    //javascripts convert null as a 0 then check 0 >= 0 so answer is true
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// ===
+console.log("2" === 2);
+
+
+
+
+
+
+
+
