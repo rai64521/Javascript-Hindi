24b83c6fa06d223429fbf4fd261a949354f33572
Sumant Rai <raisumant99@gmail.com>
2025-06-08 14:19:57 +0000

    controlflow

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -39,3 +39,13 @@ val1 = null ?? 10 ?? 20
 
 
 console.log(val1);
+
+
+
+// Ternery Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
