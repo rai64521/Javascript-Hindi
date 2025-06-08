c0f76a6274947a32c9bfef6694723d3b3cf0ded9
Sumant Rai <raisumant99@gmail.com>
2025-06-08 05:48:01 +0000

    arrays1

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,46 @@
+//Array
+
+const myArr = [2, 5, 7, 9, 4, 6, 12]
+
+const myFriends = ["Rajkumar", "Shubhangi", "pinki", "Aman"]
+
+const myArr2 = new Array(2 ,3, 6, 7, 3, 2, 10)
+
+// console.log(myArr[1]);
+// console.log(myFriends[1]);
+
+// myArr.push(15)
+// myArr.push(0)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(6));
+
+const newArr = myArr.join()
+
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof(myArr));
+
+//Slice,Splice
+
+// console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+// console.log(myn1);
+// console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+// console.log("C", myArr);
+// console.log(myn2);
+
+
+
+
+
+
+
+
