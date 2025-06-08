2c8383aec65f981429bad0000d9cd603cafe057b
Sumant Rai <raisumant99@gmail.com>
2025-06-08 03:55:28 +0000

    string methods

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,44 @@
+const name = "Sumant"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Sumant-AM')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(6));
+
+// console.log(gameName.indexOf('M'));
+
+// const newString = gameName.substring(0 , 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8 , 4)
+// console.log(anotherString);
+
+
+
+const newStringOne = " Sumant       "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://Sumant@google.com/rai%30shubhangi"
+
+console.log(url.replace('%30','-'));
+
+console.log(url.includes('shubhangi'));
+console.log(url.includes('Kush'));
+
+
+console.log(gameName.split('-'));
+
+
+
+
+
